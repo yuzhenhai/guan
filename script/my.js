19e1f3d20113b86a3f3a823ad9681ee43100ccb8
yuzhenhai <1262634632@qq.com>
2020-08-24 14:48:07 +0800

    提交

--- script/my.js
+++ script/my.js
@@ -6,3 +6,18 @@ function chosewin(){
   api.closeWin();
 
 }
+
+function yanzheng(){
+    this.src='http://dayan.yuzh.online/index.php/Api/Index/verify?'+new Date().getTime();
+}
+
+function doSearch(){
+
+}
+function msg(message){
+  api.toast({
+      msg: message,
+      duration: 2000,
+      location: 'bottom'
+  });
+}
